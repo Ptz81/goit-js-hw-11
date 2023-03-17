12e0d09d250a2b90396498b382f6e4a25aeff94c
Taras Papka <taras.papka@protonmail.com>
2023-03-17 23:25:07 +0200

    html, css basic form

---
+++ src/index.js
@@ -0,0 +1,15 @@
+import './js/visual';
+import './js/scroll';
+
+// Описаний в документації
+import SimpleLightbox from "simplelightbox";
+// Додатковий імпорт стилів
+import "simplelightbox/dist/simple-lightbox.min.css";
+
+
+/*
+
+import { fetchCountries } from './fetchCountries';
+import debounce from 'lodash.debounce';
+import Notiflix from 'notiflix';
+*/
---
+++ src/js/visual.js
@@ -0,0 +1,19 @@
+const searchElement = document.querySelector('form');
+const btnElement = document.querySelector('button');
+const inputElement = document.querySelector('input');
+const galleryElement = document.querySelector('.gallery');
+
+// function loading(e) {
+//   e.preventDefault();
+// 	search.classList.add('loading');
+
+// 	setTimeout(function() {
+// 		search.classList.remove('loading');
+// 	}, 1500);
+// }
+
+// button.addEventListener('click', loading);
+
+// input.addEventListener('keydown', function() {
+// 	if(event.keyCode == 13) loading();
+// });
